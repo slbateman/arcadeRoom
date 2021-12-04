ad2a8828984f30711ddbee4bd283de17a9013c3c
Steve Bateman <steve@steves-mac-mini.lan>
2021-12-04 16:41:18 -0700

    completed nav bar, added User component, updated routing in app.js to include User component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import AboutUs from './components/AboutUs';
-import Chat from './components/Chat';
+import Navigation from './components/Navigation';
 import Home from './components/Home';
+import Chat from './components/Chat';
+import AboutUs from './components/AboutUs';
+import User from './components/User';
 import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/login" element={<User />} />
       </Routes>
     </div>
   );
