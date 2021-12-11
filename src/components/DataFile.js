77c1664435b783a7f3e332bb6197be06f848629f
Frostypine <frostypinekng@yahoo.com>
2021-12-10 22:17:04 -0700

    working on about page

---
+++ src/components/DataFile.js
@@ -0,0 +1,22 @@
+//DataFile.js
+//Chatroom Assignment 
+//Array Bootcamp Fall 2021
+//Katie Greenwald, Steve Bateman, Bowen Condelario 
+import sloth from '../images/asloth.png';
+import bat from '../images/abat.png';
+import frostpine from '../images/afrostpine.png'; 
+
+
+const info  = 
+[
+{
+ name: "Frostypine",
+ img: frostpine,},
+{
+ name: "Sloth",
+ img: sloth,
+},
+{ 
+ name: "Slobat",
+ img: bat,},
+]
