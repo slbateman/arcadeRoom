bb737d782c8bad1ab064534acf1354a54dcd7a00
Frostypine <frostypinekng@yahoo.com>
2021-12-10 22:29:04 -0700

    updating

--- src/components/DataFile.js
+++ src/components/DataFile.js
@@ -2,7 +2,8 @@
 //Chatroom Assignment 
 //Array Bootcamp Fall 2021
 //Katie Greenwald, Steve Bateman, Bowen Condelario 
-import sloth from '../images/asloth.png';
+// import sloth from '../images/     // asloth.png';
+import sloth from '../images/'
 import bat from '../images/abat.png';
 import frostpine from '../images/afrostpine.png'; 
 
@@ -20,3 +21,5 @@ const info  =
  name: "Slobat",
  img: bat,},
 ]
+
+export default info; 
