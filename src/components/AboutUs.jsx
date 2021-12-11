f2ece4a1e31706ff9187cda65afa9ad3f77239cf
Frostypine <frostypinekng@yahoo.com>
2021-12-10 17:41:05 -0700

    tasks update

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { Card, CardGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import info from './DataFile';
+// import { useState } from 'react';
+// import { useSelector } from 'react-redux';
+// import { selectContent } from '../../state/contentSlice';
 
 function AboutUs() {
+
     return (
         <div className="about">
-            
+ <CardGroup>
+
+ {info.map((data, i) => {
+ return ( 
+
+  <Card key={i}>
+    <Card.Img variant="top" src={data.img} />
+    <Card.Body>
+      <Card.Title><Link to={`/profile/${data.name}`}>{data.name}</Link></Card.Title>
+      <Card.Text>
+      {data.bio}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    </Card.Footer>
+  </Card>
+    );
+})}
+
+  
+</CardGroup>
         </div>
-    )
+    );
 }
 
 export default AboutUs
---
+++ src/components/DataFile.js
@@ -0,0 +1,35 @@
+//DataFile.js
+//Chatroom Assignment 
+//Array Bootcamp Fall 2021
+//Katie Greenwald, Steve Bateman, Bowen Condelario 
+import sloth from '../images/asloth.png';
+import bat from '../images/abat.png';
+import frostpine from '../images/afrostpine.png'; 
+
+
+const info  = 
+[
+
+{
+ id: 0,
+ name: "Frostypine",
+ img: frostpine,
+ bio: "About Frostypine",
+},
+
+{
+ id: 1,
+ name: "Sloth",
+ img: sloth,
+ bio: "About Sloth",
+},
+
+{ 
+ id: 2,  
+ name: "Slobat",
+ img: bat,
+ bio: "About Slobat",
+},
+]
+
+export default info;
