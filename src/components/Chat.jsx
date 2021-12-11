8aa77f55c565ebb90752180323abb08f669b41c8
Frostypine <frostypinekng@yahoo.com>
2021-12-11 01:45:13 -0700

    added some file info

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,3 +1,8 @@
+//Chat.jsx
+//Chatroom Assignment 
+//Array Bootcamp Fall 2021
+//Katie Greenwald, Steve Bateman, Bowen Condelario 
+
 import React from 'react'
 
 function Chat() {
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,3 +1,8 @@
+//User.jsx
+//Chatroom Assignment 
+//Array Bootcamp Fall 2021
+//Katie Greenwald, Steve Bateman, Bowen Condelario 
+
 import React from 'react';
 import { Route, Routes } from "react-router-dom"
 import UserLogin from './UserLogin';
