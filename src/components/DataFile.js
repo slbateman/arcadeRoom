50ba1f129be6e22a49591553e06ccf89c75df703
Steve Bateman <steve@steves-mac-mini.lan>
2021-12-14 15:13:55 -0700

    basics of user profile complete

--- src/components/DataFile.js
+++ src/components/DataFile.js
@@ -1,40 +1,37 @@
 //DataFile.js
-//Chatroom Assignment 
+//Chatroom Assignment
 //Array Bootcamp Fall 2021
-//Katie Greenwald, Steve Bateman, Bowen Condelario 
+//Katie Greenwald, Steve Bateman, Bowen Condelario
 
-import sloth from '../images/asloth.png';
-import bat from '../images/abat.png';
-import frostpine from '../images/afrostpine.png'; 
-import art1 from '../images/arcadeGame.png';
-import art2 from '../images/mfGame.png';
+import sloth from "../images/asloth.png";
+import bat from "../images/abat.png";
+import frostpine from "../images/afrostpine.png";
+import art1 from "../images/arcadeGame.png";
+import art2 from "../images/mfGame.png";
 
-const info  = 
-[
+const info = [
+  {
+    id: 0,
+    name: "Frostypine",
+    img: frostpine,
+    bio: "About Frostypine",
+    art1: art1,
+    art2: art2,
+  },
 
-{
- id: 0,
- name: "Frostypine",
- img: frostpine,
- bio: "About Frostypine",
- art1: art1,
- art2: art2,
-},
+  {
+    id: 1,
+    name: "Sloth",
+    img: sloth,
+    bio: "About Sloth",
+  },
 
-{
- id: 1,
- name: "Sloth",
- img: sloth,
- bio: "About Sloth",
-},
-
-{ 
- id: 2,  
- name: "Slobat",
- img: bat,
- bio: "About Slobat",
-},
-]
+  {
+    id: 2,
+    name: "Slobat",
+    img: bat,
+    bio: "About Slobat",
+  },
+];
 
 export default info;
-
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,11 +1,31 @@
-
+import DataFile from "./DataFile";
 
 function UserProfile() {
-    return (
+  const userIndex = 2;
+  return (
+    <div className="user-profile">
+      <div className="user-profile-identification">
+        <img
+          className="user-profile-avatar"
+          src={DataFile[userIndex].img}
+          alt="avatar"
+        />
         <div>
-                        
+          <h2 className="user-profile-username">{DataFile[userIndex].name}</h2>
+          <div className="user-profile-lvl">lvl: </div>
+          <div className="user-profile-rank">rank: </div>
+          <div className="user-profile-badges">badges: </div>
         </div>
-    )
+      </div>
+      <div className="user-profile-bio">
+          <h3 className="user-profile-bio-label">bio</h3>
+          <div className="user-profile-bio-textbox">
+              <p className="user-profile-bio-text">{DataFile[userIndex].bio}</p>
+          </div>
+
+      </div>
+    </div>
+  );
 }
 
-export default UserProfile
+export default UserProfile;
