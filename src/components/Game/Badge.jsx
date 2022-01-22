d6cd1f1f8673f7eac8248fca1b370b33ae359268
Frostypine <frostypinekng@yahoo.com>
2022-01-21 18:25:23 -0700

    finished AllBadges

--- src/components/Game/Badge.jsx
+++ src/components/Game/Badge.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import '../Components.css';
-
 import badges from './badgeInfo'; 
+
 //const color = "brown"; 
 //const pic = badges[0].badge1; 
 
- function Badge(pic, color) {
+ function Badge(num1, num2) {
+ const rank = 1; 
+ const stage = 2; 
+
     return (
         <div>
         <img
           className="abadge"
-          style={{height: '2rem', width: '2rem', border: `3px solid ${color}` }}
-          src={pic}
+          style={{height: '2rem', width: '2rem', border: `3px solid ${badges[rank].color}` }}
+          src={badges[rank].badge[stage]}
           alt="badge"
         />
         
